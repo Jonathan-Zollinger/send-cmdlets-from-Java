edee6c734d82099f49bab79313bdeb98e894658a
Jonathan Zollinger <jonathan.zollinger@microfocus.com>
2020-08-12 13:43:36 -0600

    fixed errors, added comments

--- zollinger.java
+++ zollinger.java
@@ -1,33 +1,36 @@
-public static String sendCmdlet(String cmdlet){
+public static void sendCmdlet(String cmdlet){
 
-        //verify the cmdlet has a space at the end, and that no space precedes the argument
-        cmdlet = cmdlet.trim() + " ";
+        //define variables
+        String[] commandArray = command.split(" ");
+        String cmdlet = commandArray[2];
+        String line = "";
 
-        //send the command to the terminal - this page helped a lot: https://bit.ly/33PZjlB
-        Process proc = null;        //define the variable outside the try-catch to avoid IDE complaints
+        /*
+        send the command to the terminal - this page helped a lot when writing this: https://bit.ly/33PZjlB
+        be sure to dictate whether the terminal should stay open on windows with the cmd interpreter
+                https://bit.ly/3gUhHgH */
         try {
-        proc = Runtime.getRuntime().exec(cmdlet);
-        }catch (Exception e){
-        System.out.printf("attempted cmdlet failed");
-        }
+        Process proc = Runtime.getRuntime().exec(command);
 
         // Read the output
         BufferedReader reader =
-        new BufferedReader(new InputStreamReader(proc.getInputStream()));
-        String line = "";
+            new BufferedReader(new InputStreamReader(proc.getInputStream()));
         try {
-        while ((line = reader.readLine()) != null) {
-        System.out.print(line + "\n");
-        }
+            while ((line = reader.readLine()) != null) {
+            System.out.print(line + "\n");
+            }// end while ((line = reader.readLine()) != null)
         }catch (Exception e){
-        System.out.print("failed to read input stream");
+            System.out.printf("failed to read input stream from %s%n",cmdlet);
         }// end try-catch while ((line = reader.readLine()) != null)
 
         try {
-        proc.waitFor();
+            proc.waitFor();
         }catch (Exception e){
-        System.out.print("proc.waitfor() failed");
+            System.out.printf("proc.waitfor() of %s failed%n",cmdlet);
         }//end try-catch proc.waitFor();
 
-        return line;
-        }
+        }catch (Exception e){
+            System.out.printf("attempted cmdlet \"%s\" failed%n",cmdlet);
+        }//end Process proc = Runtime.getRuntime().exec(command)
+}// end  public static void sendCmdlet(String command)
+
